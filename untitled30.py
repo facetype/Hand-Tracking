314f9930687866eb7ff7dbf468614ea2d3423e55
facetype <chrtil@hotmail.com>
2025-01-09 09:24:22 +0100

    Added FPS counter
    Added FPS counter, imported pygame, still some wrong variable names will fix later

--- untitled30.py
+++ untitled30.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import mediapipe as mp
 import math
+import pygame # Will later add physics and a ball that spawns which you can balance on the line drawn with your fingers
+import time
 
 
 mpHands = mp.solutions.hands
@@ -16,8 +18,11 @@ hands = mpHands.Hands(static_image_mode=False,
 
 cap = cv2.VideoCapture(0)
 
+    #for calculating FPS
+old_frame = 0
+new_frame = 0
 
-#Check if the camera opens correctly
+    #Check if the camera opens correctly
 if not cap.isOpened():
     print("Error opening camera")
     exit()
@@ -43,6 +48,14 @@ while (1):
     results = hands.process(frame_rgb)
     
     
+        #FPS, calculating FPS by taking 1 divided by the time since the last frame. for example 1/0.05 = 20 fps
+    new_frame = time.time()
+    fps = 1/(new_frame - old_frame)
+    old_frame = new_frame
+    fps = int(fps)
+    fps = str(fps)
+    cv2.putText(frame, f"FPS {fps} ", (1750,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
     #Getting the x,y,z coordinates of the plane, 
     h, w, c = frame.shape
         
@@ -63,25 +76,48 @@ while (1):
         index_finger_y = int(hand_landmarks_1.landmark[8].y*h)
         middle_finger_x = int(hand_landmarks_2.landmark[8].x*w)
         middle_finger_y = int(hand_landmarks_2.landmark[8].y*h)
+        
+        thumb_1_x = int(hand_landmarks_1.landmark[4].x*w)
+        thumb_1_y = int(hand_landmarks_1.landmark[4].y*h)
+        thumb_2_x = int(hand_landmarks_2.landmark[4].x*w)
+        thumb_2_y = int(hand_landmarks_2.landmark[4].y*h)
+        
+            #Distance between thumbs on the X-axis
+        distance = abs(thumb_1_x - thumb_2_x)
+        
+            #If thumbs are together, draw a triangle
+        if distance < 50:
+            cv2.line(frame, (index_finger_x, index_finger_y), (thumb_1_x, thumb_1_y), (0,0,255), 5)
+            cv2.line(frame, (middle_finger_x, middle_finger_y), (thumb_2_x, thumb_2_y), (0,0,255), 5)
+        
             
             #Calculating the Euclidean distance
         distance = math.sqrt((middle_finger_x - index_finger_x)**2 + (middle_finger_y - index_finger_y)**2)
             
+            
+            #Find x-y coordinates to draw the distance on
+        x_kor = abs(index_finger_x - middle_finger_x)
+        y_kor = abs(index_finger_y - middle_finger_y)
+        
+        x_kor = index_finger_x - x_kor
+        y_kor = index_finger_y - y_kor
+            
+            
             #Displaying the distance
+        cv2.putText(frame, f"Distance: {distance:.2f} pixels", (index_finger_x + 10, index_finger_y + 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.putText(frame, f"Distance: {distance:.2f} pixels", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.line(frame, (index_finger_x, index_finger_y), (middle_finger_x, middle_finger_y), (0,0,255),5)
         
         
         
-            
+        
         
     
 
     
     # Display the processed frame
     cv2.imshow('Hand Tracking', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
     
     
     #Exit, cv2.waitKey(1) returns a 32-bit int, but the keypress is usually represented in the last
